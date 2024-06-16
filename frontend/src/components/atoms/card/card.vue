03295044a6c304420324f7ee84cb8be5906b719f
Nuno Silva <22588915+NFSS10@users.noreply.github.com>
2024-06-16 20:11:58 +0100

    Create `card` component

---
+++ frontend/src/components/atoms/card/card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card shadow-md border border-slate-200 border-solid rounded-md bg-white">
+        <div class="border-b border-slate-200 text-2xl font-medium pt-[17px] pb-[21px] px-[35px]" v-if="title">
+            {{ title }}
+        </div>
+        <div class="pt-8 pb-7 px-10">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "card",
+    props: {
+        title: String
+    }
+});
+</script>
